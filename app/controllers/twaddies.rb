a343d599cfca39bc7b02674c99804da9c70b1f77
Adellhk <adell.hk@gmail.com>
2015-02-05 15:57:02 -0800

    adds restful (but blank) controllers for twaddies and users

---
+++ app/controllers/twaddies.rb
@@ -0,0 +1,21 @@
+        # get a list of twaddies
+        get '/twaddies' do
+        end
+        # get a form to add a new twaddies
+        get '/twaddies/new' do
+        end
+        # add a new twaddies
+        post '/twaddies' do
+        end
+        # get a specific instance of twaddies
+        get '/twaddies/:id' do
+        end
+        # get a form to edit a specific instance of twaddies
+        get '/twaddies/:id/edit' do
+        end
+        # edit a specific instance of twaddies
+        put '/twaddies/:id' do
+        end
+        # delete a specific instance of twaddies
+        delete '/twaddies/:id' do
+        end
