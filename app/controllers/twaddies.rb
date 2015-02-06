29ce0e2357c0c074ef74f18e31674d009b849632
adellhk & Yukio Rattai <pair+adellhk.ukeeyo@devbootcamp.com>
2015-02-06 11:46:39 -0800

    adds seed file and some controllers and views

--- app/controllers/twaddies.rb
+++ app/controllers/twaddies.rb
@@ -4,19 +4,21 @@ get '/twaddies' do
 end
 # get a form to add a new twaddies
 get '/twaddies/new' do
+  erb :new_twaddy
 end
 # add a new twaddies
 post '/twaddies' do
+  Tweet.create(user_id: params[:session[user_id]], content: params[:message]) #user_id is not real yet
 end
 # get a specific instance of twaddies
-get '/twaddies/:id' do
-end
+# get '/twaddies/:id' do
+# end
 # get a form to edit a specific instance of twaddies
-get '/twaddies/:id/edit' do
-end
+# get '/twaddies/:id/edit' do
+# end
 # edit a specific instance of twaddies
-put '/twaddies/:id' do
-end
+# put '/twaddies/:id' do
+# end
 # delete a specific instance of twaddies
-delete '/twaddies/:id' do
-end
+# delete '/twaddies/:id' do
+# end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,9 +7,6 @@ class User < ActiveRecord::Base
   has_many :followee_relationships, class_name: 'Relationship', foreign_key: "follower_id", source: :relationships
   has_many :followees, through: :followee_relationships
 
-  # has_many :followees, through: :relationships
-
-
   has_many :tweets
 
   include BCrypt
