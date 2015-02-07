0730ab093a02e017c736079a2e56359fd6b0ac15
Dave Descheneaux <davedesch@gmail.com>
2015-02-06 16:39:21 -0800

    Fixed Everything. It's big and ugly and needs cleaning up, but it works.

--- app/controllers/twaddies.rb
+++ app/controllers/twaddies.rb
@@ -1,16 +1,16 @@
 # get a list of twaddies
-get '/twaddies' do
-  erb :twaddies
-end
-# get a form to add a new twaddies
-get '/twaddies/new' do
-  erb :new_twaddy
-end
-# add a new twaddies
-post '/twaddies' do
-  Tweet.create(user_id: 1, content: params[:message]) #user_id is not real yet
-  redirect '/twaddies'
-end
+# get '/twaddies' do
+#   erb :twaddies
+# end
+# # get a form to add a new twaddies
+# get '/twaddies/new' do
+#   erb :new_twaddy
+# end
+# # add a new twaddies
+# post '/twaddies' do
+#   Tweet.create(user_id: 1, content: params[:message]) #user_id is not real yet
+#   redirect '/twaddies'
+# end
 # get a specific instance of twaddies
 # get '/twaddies/:id' do
 # end
